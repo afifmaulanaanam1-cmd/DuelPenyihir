2ec3fd2e509773fb7d0c1b90a0f4e9889e4bfde9
afifmaulanaanam1-cmd <afifmaulanaanam1@gmail.com>
2026-02-02 11:12:18 +0700

    Update Program.cs
    Menambah Class Wizard, dua object Wizard, dan simulasi battle

--- DuelPenyihir/Program.cs
+++ DuelPenyihir/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Wizard wizardA = new Wizard("Sulis", 30);
+Wizard wizardB = new Wizard("Yinarti", 10);
+
+Console.WriteLine("Permainan Dimulai.....\n");
+wizardA.ShowStats();
+wizardB.ShowStats();
+
+wizardA.Attack(wizardB);
+wizardB.Attack(wizardA);
+wizardA.Attack(wizardB);
+
+Console.WriteLine("Permainan Berakhir....!\n");
+wizardA.ShowStats();
+wizardB.ShowStats();
+
+
+public class Wizard
+{
+    //deklarasi field 
+    public string Nama;
+    public int Energi;
+    public int Damage;
+
+    //deklarasi constructor
+    public Wizard (string nama, int damage)
+    {
+        Nama = nama;
+        Energi = 100;
+        Damage = damage;
+    }
+
+    public void ShowStats()
+    {
+        Console.WriteLine("Statistik Wizard ");
+        Console.WriteLine($"Nama : {Nama}, Energi : {Energi} \n");
+    }
+
+    public void Attack(Wizard wizardlawanOBJ)
+    {
+        //mengurangi energi wizardlawanOBJ sebesar damage
+        wizardlawanOBJ.Energi -= Damage;
+        Console.WriteLine($"{Nama} menyerang {wizardlawanOBJ.Nama}");
+        Console.WriteLine($"Sisa energi {wizardlawanOBJ.Nama} adalah {wizardlawanOBJ.Energi}\n");
+    }
+}
